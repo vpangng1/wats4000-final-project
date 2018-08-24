11d65fbc928452d54550c7cd733e48df6ea4df6f
Victor <vpangng@gmail.com>
2018-08-23 21:29:09 -0700

    2nd commit

---
+++ final-project/src/components/LoverSummary.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="lover-summary">
+    
+    <form v-on:submit.prevent="findLove">
+      <p>Find rhymes for <input type="text" v-model="blank"> related to <input type="text" v-model="blank"> <button type="submit">Calculate</button></p>
+    </form>
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+methods: {
+  
+   
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
