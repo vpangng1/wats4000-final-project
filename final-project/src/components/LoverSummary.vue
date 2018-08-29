71bb065376c8e7530811635c2cea8ff00f983c0c
Victor <vpangng@gmail.com>
2018-08-28 19:13:04 -0700

    fixed bugs and added information in index.js file

--- final-project/src/components/LoverSummary.vue
+++ final-project/src/components/LoverSummary.vue
@@ -1,34 +1,57 @@
 <template>
-  <div class="lover-summary">
-    
-    <form v-on:submit.prevent="findLove">
-      <p>Find rhymes for <input type="text" v-model="blank"> related to <input type="text" v-model="blank"> <button type="submit">Calculate</button></p>
-    </form>
-    
+  <div class="home">
+    <div v-show="showForm" class="form-container">
+      <h1>Love Calculator</h1>
+      
+
+      <p v-show="showError" class="error">There are errors in your form. Please try again.</p>
+
+      <form v-on:submit.prevent="validateForm">
+
+        
+        <p><label for="personOne">
+        <input type="text" id="personOne" v-model="personOne" placeholder="Enter your full name">
+        </label></p>
+
+        <p><label for="personTwo">
+        <input type="text" id="personTwo" v-model="personTwo" placeholder="Enter crush's full name">
+        </label></p>
+
+
+        <p><input type="submit" value="Calculate Love Compatibility"></p>
+      </form>
+    </div>
   </div>
+
+
+
 </template>
 
 <script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      personOne: '',
+      personTwo: '',
+      showForm: true,
+      showError: false
+    }
+  },
+  methods: {
+    validateForm: function () {
+      if ((this.personOne != '') && (this.personTwo != '')){
+      console.log('Form is valid');
+      this.showForm = false;
+      } else {
+      console.log('Form is NOT valid');
+      this.showError = true;
+      }
+      console.log('validating form...')
 
-	export default {
-	  name: 'LoverSummary',
-	  components: {
-	    
-	  },
-	  data () {
-	    return {
-	      
-	    }
-	  }
-	}
-
-
-
-methods: {
-  
-   
+    }
+  }
 }
-
 </script>
 
 
--- final-project/src/router/index.js
+++ final-project/src/router/index.js
@@ -1,1 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import LoverSummary from '@/components/LoverSummary'
 
+Vue.use(Router)
+
+export default new Router({
+	routes: [
+		{
+			path: '/',
+			name: 'LoverSummary',
+			component: LoverSummary
+		}
+	]
+})
